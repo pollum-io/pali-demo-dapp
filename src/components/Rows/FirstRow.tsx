c9555e07f9c7cb9b75ecd25c93d8ba45d5f49182
Amanda Gonsalves <amandagonsalves75@gmail.com>
2022-04-02 08:11:34 -0300

    chore: add sign actions

--- src/components/Rows/FirstRow.tsx
+++ src/components/Rows/FirstRow.tsx
@@ -4,11 +4,13 @@ import { Card } from '../Card';
 import { PrimaryButton, Output } from '../Button';
 
 export const FirstRow = () => (
-  <div className="bg-bkg-4 md:rounded-md grid grid-rows-2 gap-y-4 py-5 justify-center align-center w-full h-max">
+  <div className="bg-bkg-3 md:rounded-md grid lg:grid-cols-3 gap-y-4 lg:gap-y-0 md:gap-x-4 py-5 justify-center align-center w-full h-max">
     <Card title="BASIC ACTIONS">
       <div className="grid grid-rows-3 gap-y-3 rounded-full">
         <PrimaryButton text="Connect" type="button" />
         <PrimaryButton text="Get account" type="button" />
+        <PrimaryButton text="Change account" type="button" />
+        <PrimaryButton text="Disconnect" type="button" />
 
         <Output output="{}" />
       </div>
@@ -18,6 +20,15 @@ export const FirstRow = () => (
       <div className="grid grid-rows-3 gap-y-3 rounded-full">
         <PrimaryButton text="Request permissions" type="button" />
         <PrimaryButton text="Get permissions" type="button" />
+
+        <Output output="{}" />
+      </div>
+    </Card>
+
+    <Card title="SIGN ACTIONS">
+      <div className="grid grid-rows-3 gap-y-3 rounded-full">
+        <PrimaryButton text="Sign PSBT" type="button" />
+        <PrimaryButton text="Get signed PSBT" type="button" />
 
         <Output output="{}" />
       </div>
