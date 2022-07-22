26f9f92e0cccf4af11442bf867d6d7de9e813ca9
Lucas Gauer <lucasfgauer97@gmail.com>
2022-07-22 16:54:44 -0300

    chore: Create window.pali basic wrapper

---
+++ src/Pali.ts
@@ -0,0 +1,23 @@
+const { pali } = window;
+
+export const isInstalled = () => pali !== undefined;
+
+//* Event listeners
+if (isInstalled()) {
+  pali.on('connect', () => console.log('on connect'));
+  pali.on('disconnect', () => console.log('on disconnect'));
+  pali.on('accountChange', () => console.log('on accountChange'));
+  pali.on('chainChange', () => console.log('on chainChange'));
+}
+
+//* Default methods
+export const isConnected = async () => pali.isConnected();
+export const connectWallet = async () => pali.enable();
+export const disconnectWallet = async () => pali.disable();
+
+//* Requests
+export const changeAccount = async () =>
+  pali.request({ method: 'wallet_changeAccount' });
+
+export const getAccount = async () =>
+  pali.request({ method: 'sys_getAccount' });
