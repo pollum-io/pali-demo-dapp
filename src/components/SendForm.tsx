2ecefe0efc47816ca9b5acbb3e1d50d62e58c993
Lucas Gauer <lucasfgauer97@gmail.com>
2022-07-26 11:21:13 -0300

    feat: Retrieve estimated fee on load. Implement send transaction

--- src/components/SendForm.tsx
+++ src/components/SendForm.tsx
@@ -1,114 +1,120 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Form, Input } from 'antd';
 
 import QuestionCircleOutlined from '../assets/icons/QuestionCircleOutlined.svg';
+import { request } from '../Pali';
 
 import { SwitchToggle } from './Switch';
 import { PrimaryButton } from './Buttons/Button';
 import { MyDropdown } from './Dropdown';
 import { Tooltip } from './Tooltip';
 
-// export const Send = () => {
-/* const onSubmit = (data: any) => {
-    const {sender ,receiver, amount, fee } = data;
-    try {
-      ({
-        tx: {
-          fromAddress: sender,
-          toAddress: receiver,
-          amount,
-          fee,
-        },
-        type: 'sendAsset',
-      });
+const onSubmit = (data: any) => {
+  const tx = {
+    amount: Number(data.amount),
+    fee: data.fee,
+    receivingAddress: data.receiver,
+    // sender: '',
+  };
+
+  request('sys_sendTransaction', [tx]);
+};
+
+export const SendForm = () => {
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    const estimateFee = async () => {
+      const fee_ = await request('sys_estimateFee');
+      form.setFieldsValue({ fee: fee_ });
     };
-  } catch (error) {
-    alert.removeAll();
-    alert.error('An internal error has occurred.');
-  }
-  }; */
 
-export const SendForm = () => (
-  <Form
-    className="flex flex-col gap-y-3 justify-center text-center"
-    //  onFinish={onSubmit}
-  >
-    <Form.Item name="sender" className="w-full">
-      <Input
-        type="text"
-        placeholder="From"
-        className="text-white font-poppins text-sm focus:border-fields-input-borderfocus w-full outline-none focus:border focus:border-brand-royalblue bg-bkg-6 text-left px-4 py-2 border border-transparent rounded-full h-max"
-      />
-    </Form.Item>
+    estimateFee();
+  }, []);
 
-    <Form.Item name="receiver" className="w-full">
-      <Input
-        type="text"
-        placeholder="To"
-        className="text-white font-poppins text-sm focus:border-fields-input-borderfocus w-full outline-none focus:border focus:border-brand-royalblue bg-bkg-6 text-left px-4 py-2 border border-transparent rounded-full h-max"
-      />
-    </Form.Item>
+  return (
+    <Form
+      className="flex flex-col gap-y-3 justify-center text-center"
+      form={form}
+      onFinish={onSubmit}
+    >
+      <Form.Item name="sender" className="w-full">
+        <Input
+          type="text"
+          placeholder="From"
+          className="text-white font-poppins text-sm focus:border-fields-input-borderfocus w-full outline-none focus:border focus:border-brand-royalblue bg-bkg-6 text-left px-4 py-2 border border-transparent rounded-full h-max"
+        />
+      </Form.Item>
 
-    <div className="flex flex-row gap-x-4 lg:gap-x-1.5 2xl:gap-x-4 justify-center">
-      <div className="flex flex-row gap-x-2 lg:gap-x-1.5 items-center">
-        <Tooltip content="Pali verifies your address to check if it is a valid SYS address. It's useful disable this verification if you want to send to specific type of addresses, like legacy. Only disable this verification if you are fully aware of what you are doing.">
-          <img
-            src={QuestionCircleOutlined}
-            alt=""
-            className="w-4 cursor-pointer"
-          />
-        </Tooltip>
-        <p className="text-xs text-brand-royalblue font-poppins">
-          Verify Address
-        </p>
-        <SwitchToggle />
+      <Form.Item name="receiver" className="w-full">
+        <Input
+          type="text"
+          placeholder="To"
+          className="text-white font-poppins text-sm focus:border-fields-input-borderfocus w-full outline-none focus:border focus:border-brand-royalblue bg-bkg-6 text-left px-4 py-2 border border-transparent rounded-full h-max"
+        />
+      </Form.Item>
+
+      <div className="flex flex-row gap-x-4 lg:gap-x-1.5 2xl:gap-x-4 justify-center">
+        <div className="flex flex-row gap-x-2 lg:gap-x-1.5 items-center">
+          <Tooltip content="Pali verifies your address to check if it is a valid SYS address. It's useful disable this verification if you want to send to specific type of addresses, like legacy. Only disable this verification if you are fully aware of what you are doing.">
+            <img
+              src={QuestionCircleOutlined}
+              alt=""
+              className="w-4 cursor-pointer"
+            />
+          </Tooltip>
+          <p className="text-xs text-brand-royalblue font-poppins">
+            Verify Address
+          </p>
+          <SwitchToggle />
+        </div>
+
+        <div className="flex flex-row gap-x-2 lg:gap-x-1.5 items-center">
+          <Tooltip content="Disable this option for Replace-by-fee (RBF) and enable for Z-DAG, a exclusive Syscoin feature. Z-DAG enables faster transactions but should not be used for high amounts.">
+            <img
+              src={QuestionCircleOutlined}
+              alt=""
+              className="w-4 cursor-pointer"
+            />
+          </Tooltip>
+
+          <p className="text-xs text-brand-royalblue font-poppins">Z-DAG</p>
+
+          <SwitchToggle />
+        </div>
       </div>
 
-      <div className="flex flex-row gap-x-2 lg:gap-x-1.5 items-center">
-        <Tooltip content="Disable this option for Replace-by-fee (RBF) and enable for Z-DAG, a exclusive Syscoin feature. Z-DAG enables faster transactions but should not be used for high amounts.">
-          <img
-            src={QuestionCircleOutlined}
-            alt=""
-            className="w-4 cursor-pointer"
-          />
-        </Tooltip>
+      <Form.Item name="amount" className="w-full">
+        <Input
+          type="text"
+          placeholder="Amount"
+          className="text-white font-poppins text-sm focus:border-fields-input-borderfocus w-full outline-none focus:border focus:border-brand-royalblue bg-bkg-6 text-left px-4 py-2 border border-transparent rounded-full h-max"
+        />
+      </Form.Item>
 
-        <p className="text-xs text-brand-royalblue font-poppins">Z-DAG</p>
+      <Form.Item name="type" className="w-full">
+        <MyDropdown />
+      </Form.Item>
 
-        <SwitchToggle />
-      </div>
-    </div>
+      <Form.Item name="fee" className="w-full" initialValue={0.00001}>
+        <Input
+          type="text"
+          placeholder="Fee | Gas Price"
+          className="text-white font-poppins text-sm disabled:opacity-75 focus:border-fields-input-borderfocus w-full outline-none focus:border focus:border-brand-royalblue bg-bkg-6 text-left px-4 py-2 border border-transparent rounded-full h-max"
+        />
+      </Form.Item>
 
-    <Form.Item name="amount" className="w-full">
-      <Input
-        type="text"
-        placeholder="Amount"
-        className="text-white font-poppins text-sm focus:border-fields-input-borderfocus w-full outline-none focus:border focus:border-brand-royalblue bg-bkg-6 text-left px-4 py-2 border border-transparent rounded-full h-max"
-      />
-    </Form.Item>
+      <Form.Item name="data" className="w-full">
+        <Input
+          type="text"
+          placeholder="Data"
+          className="text-white font-poppins text-sm disabled:opacity-75 focus:border-fields-input-borderfocus w-full outline-none focus:border focus:border-brand-royalblue bg-bkg-6 text-left px-4 py-2 border border-transparent rounded-full h-max"
+        />
+      </Form.Item>
 
-    <Form.Item name="type" className="w-full">
-      <MyDropdown />
-    </Form.Item>
-
-    <Form.Item name="gas price" className="w-full">
-      <Input
-        type="text"
-        placeholder="Gas Price"
-        className="text-white font-poppins text-sm disabled:opacity-75 focus:border-fields-input-borderfocus w-full outline-none focus:border focus:border-brand-royalblue bg-bkg-6 text-left px-4 py-2 border border-transparent rounded-full h-max"
-      />
-    </Form.Item>
-
-    <Form.Item name="data" className="w-full">
-      <Input
-        type="text"
-        placeholder="Data"
-        className="text-white font-poppins text-sm disabled:opacity-75 focus:border-fields-input-borderfocus w-full outline-none focus:border focus:border-brand-royalblue bg-bkg-6 text-left px-4 py-2 border border-transparent rounded-full h-max"
-      />
-    </Form.Item>
-
-    <PrimaryButton text="Submit" type="submit">
-      Next
-    </PrimaryButton>
-  </Form>
-);
+      <PrimaryButton text="Submit" type="submit">
+        Next
+      </PrimaryButton>
+    </Form>
+  );
+};
