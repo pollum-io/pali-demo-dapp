eab5dc08256869596200b47f8617ab5e6b88e1ee
Lucas Santos <lucasgabrielgtx@gmail.com>
2022-10-03 13:25:48 -0300

    fix: solve output overflow bug

--- src/components/Output.tsx
+++ src/components/Output.tsx
@@ -5,7 +5,7 @@ interface IOutput {
 }
 
 export const Output: React.FC<IOutput> = ({ output }) => (
-  <div className="border border-bkg-4 bg-bkg-6 font-poppins text-left p-2 rounded-md outline-none overflow-x">
+  <div className="border border-bkg-4 w-full bg-bkg-6 font-poppins text-left p-2 rounded-md outline-none overflow-x-auto">
     <pre>{output}</pre>
   </div>
 );
